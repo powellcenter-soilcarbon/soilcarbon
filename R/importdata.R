75ed96624048806b7c2016a6039a8d0d886146e4
Grey Monroe <greymonroe@gmail.com>
2017-01-28 13:27:29 -0700

    more files from last commit

--- R/importdata.R
+++ R/importdata.R
@@ -1,4 +1,4 @@
-#'importdata
+#' importdata
 #'
 #' This function imports data from xlsx format matching the standard soil carbon data template
 #'
@@ -6,11 +6,25 @@
 #' @import XLConnect
 #' @export
 #' @examples
-#'
-#' importdata(file="~/path/to/file/file.xlsx")
+#' datafile<-system.file("extdata", "UNALTERED_TEMPLATE_v120616.xlsx", package = "soilcarbon")
+#' importdata(file=datafile)
 
 importdata<-function(file){
-  require(XLConnect)
   data_workbook<-loadWorkbook(file)
+  sheets_found<-getSheets(data_workbook)
+  sheets_needed<-c("metadata","site","profile","layer", "fraction")
+  if (F %in% (sheets_needed %in% sheets_found)){
+    sheets_missing<-setdiff(sheets_needed, sheets_found)
+    stop(paste("Sheet(s) '",sheets_missing,"' missing from data file", sep=""))
+  }
+
+  metadata<-readWorksheet(object=data_workbook , sheet="metaadata", header = TRUE )
+  site<-readWorksheet(object=data_workbook , sheet="site", header = TRUE )
+  profile<-readWorksheet(object=data_workbook , sheet="profile", header = TRUE )
+  layer<-readWorksheet(object=data_workbook , sheet="layer", header = TRUE )
+  fraction<-readWorksheet(object=data_workbook , sheet="fraction", header = TRUE )
+
+  data_workbook=list(metadata=metadata, site=site, profile=profile, layer=layer, fraction=fraction)
   return(data_workbook)
 }
+
