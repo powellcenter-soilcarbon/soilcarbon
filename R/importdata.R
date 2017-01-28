2ebba92452812152d7eafb06a7da1f431e9f2e85
Grey Monroe <greymonroe@gmail.com>
2017-01-28 14:38:53 -0700

    fixed bug in importdata functon

--- R/importdata.R
+++ R/importdata.R
@@ -10,7 +10,7 @@
 #' importdata(file=datafile)
 
 importdata<-function(file){
-  data_workbook<-loadWorkbook(file)
+  data_workbook<-loadWorkbook(testdata_directory)
   sheets_found<-getSheets(data_workbook)
   sheets_needed<-c("metadata","site","profile","layer", "fraction")
   if (F %in% (sheets_needed %in% sheets_found)){
@@ -18,7 +18,7 @@ importdata<-function(file){
     stop(paste("Sheet(s) '",sheets_missing,"' missing from data file", sep=""))
   }
 
-  metadata<-readWorksheet(object=data_workbook , sheet="metaadata", header = TRUE )
+  metadata<-readWorksheet(object=data_workbook , sheet="metadata", header = TRUE )
   site<-readWorksheet(object=data_workbook , sheet="site", header = TRUE )
   profile<-readWorksheet(object=data_workbook , sheet="profile", header = TRUE )
   layer<-readWorksheet(object=data_workbook , sheet="layer", header = TRUE )
