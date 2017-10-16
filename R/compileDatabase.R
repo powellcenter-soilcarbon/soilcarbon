3f830e36d1d7392d2ea89434826aeaf64c2d6593
Grey Monroe <greymonroe@gmail.com>
2017-10-16 14:13:15 -0600

    removed database from package updated compile function

--- R/compileDatabase.R
+++ R/compileDatabase.R
@@ -9,12 +9,14 @@
 
 compileDatabase <- function(dataset_directory ){
 
-  data_files<-list.files( dataset_directory, full.names = T	)
+  data_files<-list.files(dataset_directory, full.names = T	)
 
-  #special dataset (Yujie)
-  Yujie_database <- NULL; rm(Yujie_database)
-  working_database<-Yujie_database
-  working_database[] <- lapply(working_database, as.character)
+  # special dataset (Yujie)
+  # Yujie_database <- NULL; rm(Yujie_database)
+  # working_database<-Yujie_database
+  # working_database[] <- lapply(working_database, as.character)
+
+  working_database<-data.frame()
 
   for(i in 1:length(data_files)){
     soilcarbon_data<-read.soilcarbon(data_files[i])
@@ -27,6 +29,6 @@ compileDatabase <- function(dataset_directory ){
   working_database[]<-lapply(working_database, type.convert)
   soilcarbon_database<-working_database
 
-  use_data(soilcarbon_database, overwrite = T)
+  return(soilcarbon_database)
 
 }
