7a12565321b85077e166c068cf07dc220086aeef
Grey Monroe <greymonroe@gmail.com>
2017-10-22 17:41:47 -0600

    updates

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -4,7 +4,26 @@ library(ggplot2)
 shinyServer(function(input, output, session) {
   options(shiny.maxRequestSize=30*1024^2)
 
+  observe({
+    x <- input$presets
 
+    if (x==1) {
+      updateSelectInput(session, "y_var", selected = "layer_bot")
+      updateSelectInput(session, "x_var", selected = "bd_tot")
+      updateSelectInput(session, "size_var", selected = "NULL")
+      updateSelectInput(session, "col_var", selected = "mat")
+      updateSelectInput(session, "col_facet_var", selected = "map")
+      updateSelectInput(session, "row_facet_var", selected = "mat")
+    }
+    if (x==2) {
+      updateSelectInput(session, "y_var", selected = "f_c_tot")
+      updateSelectInput(session, "x_var", selected = "f_14c")
+      updateSelectInput(session, "size_var", selected = "NULL")
+      updateSelectInput(session, "col_var", selected = "f_property")
+      updateSelectInput(session, "col_facet_var", selected = "NULL")
+      updateSelectInput(session, "row_facet_var", selected = "NULL")
+    }
+  })
 
   output$plot <- renderPlot({
 
@@ -50,7 +69,6 @@ shinyServer(function(input, output, session) {
     }else{
       p<-ggplot(plot_data, aes_string(x=variables$x_var, y=variables$y_var, col=variables$col_var))+
         facet_grid(facet_cut2~facet_cut)+
-        scale_colour_gradient(low="dodgerblue", high="orange")+
         theme_light(base_size = 16)+
         theme(strip.background = element_blank())+
         theme(strip.text = element_text(colour = 'black'))
