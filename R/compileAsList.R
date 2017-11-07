8cb627f841e88c1ad6394b95b607e2aef303219f
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2017-11-07 17:24:33 +0100

    Created a function to read the database as a list of lists

---
+++ R/compileAsList.R
@@ -0,0 +1,15 @@
+#' compileAsList
+#'
+#' Compiles all entries (xlsx files) and creates a version of the database as a list
+#'
+#' @param dataset_directory directory where compeleted and QC passed soilcarbon datasets are stored
+
+compileAsList = function(dataset_directory){
+  entryNames=list.files(dataset_directory, full.names=FALSE, recursive=FALSE)
+  shortNames=(strsplit(entryNames,split=".xlsx"))
+  paths=paste(dataset_directory,entryNames, sep="")
+
+  out=lapply(paths,read.soilcarbon)
+  return(out)
+}
+
