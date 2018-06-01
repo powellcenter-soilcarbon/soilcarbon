3222fb89b26a394b2d46ccb67c9c059826ae5319
Jeff B <jbeemmil@gmail.com>
2018-06-01 14:21:29 +0200

    Updated compileAsList
    Now works with non-excel files in directory.

---
+++ compileAsList.R
@@ -0,0 +1,16 @@
+#' compileAsList
+#'
+#' Compiles all entries (xlsx files) and creates a version of the database as a list
+#'
+#' @param dataset_directory directory where compeleted and QC passed soilcarbon datasets are stored
+#' @export
+
+compileAsList = function(dataset_directory){
+  entryNames=list.files(dataset_directory, full.names=FALSE, recursive=FALSE)
+  entryNames=entryNames[grep("xlsx", entryNames)]
+  shortNames=(strsplit(entryNames,split=".xlsx"))
+  paths=paste(dataset_directory,entryNames, sep="")
+
+  out=lapply(paths,read.soilcarbon)
+  return(out)
+}
